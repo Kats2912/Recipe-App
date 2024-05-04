87978cc4cc1a745d48bbd0d527bcbd89b6332cb5
Kartik Gupta <cooldude.kartik.123@gmail.com>
2024-05-04 11:40:27 +0530

    first commit

---
+++ src/components/InputBar/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import Recipies from "../Recipies";
+import useSWR from "swr";
+
+const InputBar = () => {
+  const [recipe, setRecipe] = useState("");
+
+  //function for fetcher for handling apis
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const {
+    data: countries,
+    error,
+    isValidating,
+  } = useSWR("https://restcountries.com/v2/all", fetcher);
+  // Handles error and loading state
+  if (error) return <div className="failed">failed to load</div>;
+  else if (isValidating) return <div className="Loading">Loading...</div>;
+  else return (
+      <>
+          
+      <Form.Group className="mb-3">
+        <Form.Label>Search Recipe:</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Chicken Tikka"
+          value={recipe}
+          onChange={(e) => {
+            setRecipe(e.target.value);
+          }}
+        />
+      </Form.Group>
+          {countries && countries.map((country) => {
+              console.log(country);
+      })}
+      <Recipies />
+    </>
+  );
+};
+
+export default InputBar;
